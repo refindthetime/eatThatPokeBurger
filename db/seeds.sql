b37c027794fa1fa0cba5e4899801626fd59970c4
Derek Cassel <derekdcassel@gmail.com>
2017-09-30 14:57:10 -0600

    commits made to Pokeburger

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name) VALUES ('PikaBurger');
+INSERT INTO burgers(burger_name) VALUES ('CharmandaBurger');
+INSERT INTO burgers(burger_name) VALUES ('BulbaBurger');
+INSERT INTO burgers(burger_name) VALUES ('Magikarp Cheeseburger');
+INSERT INTO burgers(burger_name) VALUES ('Pidgeyburger');
